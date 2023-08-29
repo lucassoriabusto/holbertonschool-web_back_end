69d49c2919b8f251b44892eb172468b56ea2eb9c
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-28 21:20:01 -0700

    Task 01

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,6 +3,6 @@
 takes a string str1 and a string str2 """
 
 
-def concat(str1: str,str2: str)-> str:
+def concat(str1: str, str2: str) -> str:
     """ Returns a concatenated string """
     return str1 + str2
