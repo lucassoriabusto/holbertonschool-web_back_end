919e2c837267a1b8c80cf786dcc7e4192618d8fd
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-29 13:12:04 -0700

    Task 07

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """ Type-annotated function to_kv that takes a string k and
 an int OR float v as arguments and returns a tuple."""
-from typing import Union
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Return a tuple """
-    new_tuple = ()
     square_v = v ** 2
-    new_tuple = (k, float(square_v))
+    new_tuple = (k, square_v)
     return new_tuple
