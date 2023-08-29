92b8a9f1058c2888328de09e00b396a161325b8e
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-29 14:17:45 -0700

    Task 09

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Annotate the below function’s parameters
+and return values with the appropriate types """
+from typing import List, Tuple
+
+
+def element_length(lsti: List[str]) -> List[Tuple[str, int]]:
+    return [(i, len(i)) for i in lst]
