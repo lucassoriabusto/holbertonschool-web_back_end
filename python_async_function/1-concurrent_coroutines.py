8c9c37004f86ed1a66a87872a408596a85721a0d
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-30 21:11:37 -0700

    Task 01

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" """
+""" Importa la funcion wait_random """
 
 from typing import List
 
@@ -7,7 +7,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-
+    """ Espera n veces utilizando wait_random con
+    un tiempo de espera máximo especificado. """
     new_list = []
     for i in range(n):
         num = await wait_random(max_delay)
