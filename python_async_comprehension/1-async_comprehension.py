ee56fc3a20449e1a0557fac3e18765e2126ca974
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-31 18:02:26 -0700

    Task 01

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" """
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> List[float]:
+    random_numbers = [num async for num in async_generator()]
+    return random_numbers
