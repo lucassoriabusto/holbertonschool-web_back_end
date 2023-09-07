8c3d003e61ba1b8d9c04b9449415b5a32e6dde1e
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-09-07 14:51:10 -0700

    Task 00

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """ Function that returns a tuple containing
 the start index and the end index """
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """ Calculates start rate and end rate """
     start_index = (page - 1) * page_size
