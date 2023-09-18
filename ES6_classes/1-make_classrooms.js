3c172a556bc10b05f83129c9d51cc5984e921a44
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-09-17 18:36:16 -0700

    Task 01

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,11 +1,11 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-	const class1 = new ClassRoom(19);
-	const class2 = new ClassRoom(20);
-	const class3 = new ClassRoom(34);
-	const rooms = [class1, class2, class3];
-	return rooms;
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+  const rooms = [class1, class2, class3];
+  return rooms;
 }
 
 initializeRooms();
