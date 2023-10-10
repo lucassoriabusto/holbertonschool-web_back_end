4bded3239276475e10de2fccfec530208071810c
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-10-10 13:15:26 -0700

    Task 02

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(studentList, city) {
-  const studentCity = studentList.filter(student => student.location === city);
+  const studentCity = studentList.filter((student) => student.location === city);
   return studentCity;
 }
