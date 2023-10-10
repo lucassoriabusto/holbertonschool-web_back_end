47b38af9548df7edbb562eb83199d5613d049d2c
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-10-10 09:59:41 -0700

    Task 00

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,25 +1,25 @@
-export default function getListStudents () {
+export default function getListStudents() {
   const studentList = [];
 
   const student1 = {
     id: 1,
-    firstName: "Guillaume",
-    location: "San Francisco"
-  }
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
 
   studentList.push(student1);
 
   const student2 = {
     id: 2,
-    firstName: "James",
-    location: "Columbia"
+    firstName: 'James',
+    location: 'Columbia',
   };
   studentList.push(student2);
 
   const student3 = {
     id: 5,
-    firstName: "Serena",
-    location: "San Francisco"
+    firstName: 'Serena',
+    location: 'San Francisco',
   };
   studentList.push(student3);
 
