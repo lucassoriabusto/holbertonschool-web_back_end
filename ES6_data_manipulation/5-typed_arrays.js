bd30d451dfb29819ec3be39a70b749367351a4ad
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-10-11 10:32:41 -0700

    Task 05

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
-export default createInt8TypedArray(length, position, value) {
+export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    throw new Error("Position outside range");
+    throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
   const int8Array = new Int8Array(buffer);
 
   int8Array[position] = value;
 
-  return int8Array
+  return int8Array;
 }
