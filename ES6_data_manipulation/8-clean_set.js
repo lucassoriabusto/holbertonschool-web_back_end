25986c7fbe0f30d7315ea9f57e405e3f26657b75
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-10-12 19:04:23 -0700

    Task 08

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,17 @@
 export default function cleanSet(set, startString) {
   let result = '';
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      const restOfValue = value.slice(startString.length);
-      if (result) {
-        result += `-${restOfValue}`;
-      } else {
-        result += restOfValue;
+  if (startString) {
+    for (const value of set) {
+      if (value.startsWith(startString)) {
+        const restOfValue = value.slice(startString.length);
+        if (result) {
+          result += `-${restOfValue}`;
+        } else {
+          result += restOfValue;
+        }
       }
     }
+    return result;
   }
   return result;
 }
