100834b56e79d1d5da3720443f45f6e756d9c888
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-10-13 12:04:17 -0700

    Task 08

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   let result = '';
   if (typeof startString === 'string' && startString) {
     for (const value of set) {
-      if (value.startsWith(startString)) {
+      if (value && typeof value === 'string' && value.startsWith(startString)) {
         const restOfValue = value.slice(startString.length);
         if (result) {
           result += `-${restOfValue}`;
