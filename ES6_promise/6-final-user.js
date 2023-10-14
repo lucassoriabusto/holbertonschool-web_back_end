65e48bc134cb977fe080db197df3912ba14ca073
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-10-14 16:43:35 -0700

    Task 06

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -8,6 +8,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([userPromise, photoPromise])
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value: result.value, // Corregir aquí
     })));
 }
